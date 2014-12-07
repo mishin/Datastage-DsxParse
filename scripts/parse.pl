fda11b4249f43f6870cad35035169d503482c92b
Nikolay Mishin <mi@ya.ru>
2014-12-08 00:25:41 +0300

    make regexp for orchestrate with https://regex101.com/

--- scripts/parse.pl
+++ scripts/parse.pl
@@ -1,10 +1,16 @@
 use Modern::Perl;
 use Datastage::DsxParse qw(debug parse_dsx);
 use Carp::Always;
-use Data::Dumper;
-my $file_name=shift or die "Usage: $0 dsx_file_name\n";
-print Dumper $file_name;
-my $dsx_structure=parse_dsx($file_name);
+use Data::TreeDumper;
+#use re 'debug';
+
+#use Data::Dumper;
+my $file_name = shift or die "Usage: $0 dsx_file_name\n";
+
+#print Dumper $file_name;
+my $dsx_structure = parse_dsx($file_name);
+
 #@parse_dsx=(1,2);
 #debug(1,\@parse_dsx);
-debug(1,$dsx_structure);
+debug( 1, $dsx_structure );
+print DumpTree( $dsx_structure, '$dsx_structure' );
