83364c5039d876906f9c1ee710a1d4639410e84e
Nikolay Mishin <mi@ya.ru>
2014-12-09 01:25:32 +0300

    deep walk

--- scripts/parse.pl
+++ scripts/parse.pl
@@ -3,13 +3,14 @@ use Datastage::DsxParse qw(debug parse_dsx);
 use Carp::Always;
 use Data::TreeDumper;
 #use re 'debug';
+use Data::Dumper;
 
-#use Data::Dumper;
+
 my $file_name = shift or die "Usage: $0 dsx_file_name\n";
 
-#print Dumper $file_name;
-my $dsx_structure = parse_dsx($file_name);
 
+my $dsx_structure = parse_dsx($file_name);
+#print Dumper $dsx_structure;
 #@parse_dsx=(1,2);
 #debug(1,\@parse_dsx);
 #debug( 1, $dsx_structure );
