dc3f9ea0878b2452226acaaec757c8bfc809f687
Nikolay Mishin <mi@ya.ru>
2014-12-17 09:59:21 +0300

    Update parse.pl

--- scripts/parse.pl
+++ scripts/parse.pl
@@ -1,20 +1,19 @@
 use Modern::Perl;
-use lib "/home/mishin/github/Datastage-DsxParse/lib";
-#use FindBin;                 # locate this script
-#use lib "$FindBin::Bin/..";  # use the parent directory
+use Config;
+if ($Config{osname} eq 'linux') {
+    use lib "/home/mishin/github/Datastage-DsxParse/lib";
+}
+elsif ($Config{osname} eq 'MSWin32') {
+    use lib
+      "c:/Users/rb102870/Documents/job/bin/dsx/projects/Datastage-DsxParse-master/lib";
+}
+say $Config{osname};
 use Datastage::DsxParse qw(debug parse_dsx);
 use Carp::Always;
 use Data::TreeDumper;
-#use re 'debug';
-use Data::Dumper;
-
 
 my $file_name = shift or die "Usage: $0 dsx_file_name\n";
 
 
 my $dsx_structure = parse_dsx($file_name);
-#print Dumper $dsx_structure;
-#@parse_dsx=(1,2);
-#debug(1,\@parse_dsx);
-debug( 1, $dsx_structure );
-#print DumpTree( $dsx_structure, '$dsx_structure' );
+debug(1, $dsx_structure);
