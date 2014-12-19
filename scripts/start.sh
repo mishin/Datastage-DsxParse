d8802e3337fee7d69b9641ffad42e57d8639c900
Nikolay Mishin <mi@ya.ru>
2014-12-20 00:11:35 +0300

    automate shell script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
-perl parse.pl data/example.dsx > example.log
-perl t/01_compare_log.t example.log.orig example.log
+perl parse.pl $1 > $1.log 2>$1.err.log
+#perl t/01_compare_log.t example.log.orig example.log
