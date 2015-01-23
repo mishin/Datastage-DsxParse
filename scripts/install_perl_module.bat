26be50d0fc987faa938c48f012557db62cf7e7b4
Nikolay Mishin <mi@ya.ru>
2015-01-23 10:41:19 +0300

    Update install_perl_module.bat

--- scripts/install_perl_module.bat
+++ scripts/install_perl_module.bat
@@ -1,7 +1,4 @@
-rem set file_src=DateTime-Format-Strptime-1.56.tar.gz
-REM set file_src=DateTime-TimeZone-1.83.tar.gz
-set file_src=File-Slurp-Tiny-0.003.tar.gz
-REM set file_src=%1
+set file_src=%1
 call ptar -x -f %file_src%
 perl -e "if ($ARGV[0]=~ /(.*)([.]tar[.]gz|[.]tgz)$/){print $1}" %file_src% > tmpFile
 set /p dir_name= < tmpFile
