bfc54aab23907c992d7e3ad431844eb583ede65c
Nikolay Mishin <mi@ya.ru>
2015-08-15 03:53:00 +0300

    Update add_text.pl

--- scripts/add_text.pl
+++ scripts/add_text.pl
@@ -3,8 +3,8 @@ my $text='BEGIN HEADER
    CharacterSet "CP1251"
    ExportingTool "IBM InfoSphere DataStage Export"
    ToolVersion "8"
-   ServerName "PROD-ETL"
-   ToolInstanceID "EKFO"
+   ServerName "TEST-ETL"
+   ToolInstanceID "FFF"
    MDISVersion "1.0"
    Date "2014-12-18"
    Time "12.05.57"
@@ -13,6 +13,6 @@ END HEADER
 BEGIN DSJOB'."\n";
 
 
-my $my_dir= 'c:/Users/rb102870/Documents/job/bin/dsx/projects/Datastage-DsxParse-master/scripts/data/CRZ_GR1';
+my $my_dir= 'c:/Users/325/Documents/job/bin/dsx/projects/Datastage-DsxParse-master/scripts/data/333';
  my @files = read_dir($my_dir) ;
 prepend_file( $_, $text )  for grep(/^mail/, @files);
